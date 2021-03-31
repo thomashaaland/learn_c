b82e51f63e88507263110e7a340aff274e96fd7b
thomashaaland <thomas.haaland@gmail.com>
2021-03-31 20:26:08 +0200

    Can change mem allocation

--- mem_manage/name.c
+++ mem_manage/name.c
@@ -9,16 +9,26 @@ int main() {
 
   strcpy(name, "Lars Larson");
 
-  description = malloc(200 * sizeof(char));
+  description = malloc(20 * sizeof(char));
 
   if (description == NULL) {
     fprintf(stderr, "Error - unable to allocate required memory\n");
   } else {
-    strcpy( description, "Lars is a great guy!");
+    strcpy( description, "Lars is a great guy! ");
   }
 
+  description = realloc( description, 100 * sizeof(char));
+
+  if (description == NULL) {
+    fprintf(stderr, "Error - unable to allocate required memory\n");
+  } else {
+    strcat(description, "Lars really is the best!");
+  }
+  
   printf("Name = %s\n", name);
   printf("Description: %s\n", description);
+
+  free(description);
 
   return 0;
 }
